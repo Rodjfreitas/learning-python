e6956c82adcbf87272922f42d884526c1eac77c6
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-02 12:39:39 -0300

    docs: melhoria no exercicio de notas fiscais

--- Testes/ExercicioNFe.py
+++ Testes/ExercicioNFe.py
@@ -8,16 +8,27 @@ irrf = notavalor * 0.015
 inss = notavalor * 0.11
 icms = notavalor * 0.18
 totalimpostos = pis + cofins + csll + irrf + inss + icms
+federais = [pis, cofins, csll, irrf, inss]
+impostosfederais = 0
+
+i = 0
+while i < len(federais):
+    impostosfederais += federais[i]
+    i += 1
 
 print('{:=^30}'.format('NOTA FISCAL'))
 
 print('\nValor Total: R$ {:.2f}'.format(notavalor))
 
-print('PIS/COFINS/CSLL: {:.2F}'.format((pis + cofins + csll)))
+print('\nPIS/COFINS/CSLL: {:.2F}'.format((pis + cofins + csll)))
 print('IRRF: R$ {:.2f}'.format(irrf))
 print('INSS: R$ {:.2f}'.format(inss))
-print('ICMS: R$ {:.2f}'.format(icms))
+print('\nTotal de impostos federais: R$ {:.2f}'.format(impostosfederais))
+print('\nICMS: R$ {:.2f}'.format(icms))
+
 
 print('Valor Líquido: R$ {:.2f}\n'.format(notavalor - totalimpostos))
 
-print('{:=^30}'.format('='))
+print('\nObservação: O valor líquido da nota equivale à {:.0f}% do valor total'.format((notavalor - totalimpostos)/notavalor * 100))
+
+print('\n{:=^30}'.format('='))
