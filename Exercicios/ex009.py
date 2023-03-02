4d74f806bfb53e5ab0ee44e39c00be2c7ca79bab
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-02 11:35:33 -0300

    docs: brincando com exercicio 09. utilizando varias tecnicas de aprendizado

--- Exercicios/ex009.py
+++ Exercicios/ex009.py
@@ -3,8 +3,10 @@
 numero = int(input('Digite um número inteiro: '))
 
 print('A tabuada de {} é: '.format(numero))
-
+print('{:=^15}'.format('='))
 i = 0
 while i < 11:
-    print('{} x {} = {}'.format(numero, i, numero * i))
+    print('{:^5} x {:^2} = {:<10} - {:<20}'.format(numero, i, (numero * i), ('x' * (numero *i))))
     i += 1
+
+print('{:=^15}'.format('='))
