45ab6d76eb81531a3d4eba3d9f350b360b272c20
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-05 22:01:38 -0300

    feat: completando cadastros

--- Modulos/cadastro.py
+++ Modulos/cadastro.py
@@ -19,3 +19,21 @@ i = 0
 while i < len(cadastro):
     print('\nNome: {} / Cidade: {} / Nascimentos: {}'.format(cadastro[i][0], cadastro[i][1], cadastro[i][2]))
     i += 1
+
+
+pergunta2 = int(input('\nGostaria de Realizar alguma pesquisa?\n[1] - Sim\n[2] - Não '))
+
+if pergunta2 < 1 or pergunta2 > 2:
+    pergunta2 = int(input('\nInválido!\nGostaria de Realizar alguma pesquisa?\n[1] - Sim\n[2] - Não '))
+
+pesquisa = int(input('\nPesquisar por:\n[0] - Nome\n[1] - Cidade\n[2] - Nascimento'))
+if pesquisa < 0 or pesquisa > 2:
+    pesquisa = int(input('\nInválido!\nPesquisar por:\n[0] - Nome\n[1] - Cidade\n[2] - Nascimento\n'))
+tipoPesq = input('\nDigite a Palavra chave: ')
+i = 0
+if pergunta2 == 1:
+    while i < len(cadastro):        
+        if cadastro[i][pesquisa] == tipoPesq:
+            print(cadastro[i])
+        i += 1
+
