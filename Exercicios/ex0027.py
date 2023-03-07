c459353d8a98f83561a12312b01575e99169dd51
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-07 11:47:06 -0300

    feat: exercicio 27

---
+++ Exercicios/ex0027.py
@@ -0,0 +1,10 @@
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nome = str(input('\nDigite seu nome completo: '))
+nomeSeparado = nome.split()
+qtdespacos = len(nomeSeparado) - 1
+
+while qtdespacos < 2:
+    nome = str(input('\nInválido. Digite seu nome completo: '))
+
+print('\nSeja bem vindo {} {}'.format(nomeSeparado[0], nomeSeparado[qtdespacos]))
