46c41324a292fefc1d27c60d5efeb362cda93bb4
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-08 11:21:45 -0300

    feat: incrementação do exercicio 22

--- Exercicios/ex0022.py
+++ Exercicios/ex0022.py
@@ -5,12 +5,15 @@ nome = input('\nDigite seu nome completo: ')
 while nome.isalnum() == True:
     nome = input('\nInválido. Digite seu nome completo: ')
 
-print(nome.upper())
-print(nome.lower())
+print('seu nome maiusculo é: {}'.format(nome.upper()))
+print('seu nome minusculo é {}:'.format(nome.lower()))
 # conta a quantidade total de caracteres digitadas, incluindo espaços
 totalcaracteres = len(nome)
 totalcaracteres = int(totalcaracteres)
 # conta a quantidade de palavras dentro da variavel para que possa identificar a quantidade de espaços
 espacos = len(nome.split())
 espacos = int(espacos -1)
-print('Quantidade de Caracteres: {}'.format(totalcaracteres - espacos))
+
+primeiroNome = nome.split()
+print('seu nome tem ao todo {} letras'.format(totalcaracteres - espacos))
+print('Seu primeiro nome é {} e tem {} letras.'.format(primeiroNome[0], len(primeiroNome[0])))
