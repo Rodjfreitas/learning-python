88abc9adeae16115f4c01138db9352ab1d6eccac
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-10 22:38:10 -0300

    feat: exercicio 37

---
+++ Exercicios/ex0037.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octa e 3 para hexadecimal
+
+numero = 5
+
+print(bin(numero))
