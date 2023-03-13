5a58a275b9731dad265d7025d2ff7089fd807ebb
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-13 13:59:48 -0300

    feat: correção exercicio 50

--- Exercicios/ex0050.py
+++ Exercicios/ex0050.py
@@ -6,6 +6,6 @@ for c in range(1, 7):
     if numero % 2 == 0:
         soma += numero
         numeros.append(numero)
-print('\nA soma dos números é \033[1;30;42m{}\033[m'.format(soma))
+print('\nA soma dos números é \033[1;30;42m {} \033[m'.format(soma))
 print('\nOs números considerados foram {}'.format(numeros))
     
