e2f1a677b40be5f692cf1c9570c3403994d08645
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-16 14:51:33 -0300

    feat: programa exportar de excel para word

---
+++ ExportarExcelWord/projeto.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from docx import Document
+from docx.shared import Inches
+
+confirmacao = input('Deseja iniciar:\n[1] - Sim\n[2] - Sair\n')
+
+if int(confirmacao) == 1:
+
+    # Importa os dados da planilha em formato de DataFrame
+    df = pd.read_excel('RelatorioExemplos.xlsx')
+
+    # Cria um novo arquivo word
+    document = Document()
+
+    # Adiciona título ao arquivo word
+    document.add_heading('Relatório', 0)
+
+    # Adiciona uma tabela com os dados da planilha ao arquivo word
+    table = document.add_table(rows=len(df) + 1, cols=len(df.columns))
+    for i in range(len(df.columns)):
+        table.cell(0,i).text = df.columns[i]
+    for i in range(len(df)):
+        for j in range(len(df.columns)):
+            table.cell(i + 1, j).text = str(df.values[i, j])
+
+    # salva o arquivo do word
+    document.save('arquivo_word.docx')
+    print('{:=^50}'.format('Arquivo Gerada'))
+else:
+    print('{:=^50}'.format('Fim, Nada foi Executado.'))
