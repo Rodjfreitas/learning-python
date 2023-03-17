0ee66ea994f1a6521ae9f641997d5ba799716939
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-17 20:01:02 -0300

    feat: interrompendo while - exercicio 67 tabuada

---
+++ Exercicios/ex0067.py
@@ -0,0 +1,11 @@
+# Faça um programa que mostre a tabuada de várias números, um de cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negatívo.
+while True:
+    numero = int(input('\nQuer ver a tabuada de qual valor? '))
+    if numero < 0:
+        print(f'\n{"FIM":=^50}\n')
+        break
+    i = 0
+    while i < 11:
+        print(f'{numero} x {i} = {numero * i}')
+        i += 1
+    print(f'{"=":=^50}')
