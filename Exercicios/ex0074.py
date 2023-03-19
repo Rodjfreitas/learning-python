1c55d30febd5db58e44498f6cbf11771da47dade
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-19 13:12:37 -0300

    feat: tuplas - exercicio 74 nova maneira de fazer

--- Exercicios/ex0074.py
+++ Exercicios/ex0074.py
@@ -1,18 +1,22 @@
-from random import randint
+from random import sample
 # crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. depois disso mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
-numeros = (randint(0, 50), randint(0, 50), randint(
-    0, 50), randint(0, 50), randint(0, 50))
-maior = 0
-menor = 0
-for pos, numero in enumerate (numeros):
-    if pos == 0:
-        maior = numero
-        menor = numero
-    else:
-        if numero > maior:
-            maior = numero
-        if numero < menor:
-            menor = numero
+numeros = (sample(range(0, 50), 5))
+#maior = 0
+#menor = 0
+# Método com enumerate
+#for pos, numero in enumerate (numeros):
+#    if pos == 0:
+#        maior = numero
+#        menor = numero
+#    else:
+#        if numero > maior:
+#            maior = numero
+#        if numero < menor:
+#            menor = numero
+#print(f'\nOs números sorteados foram: {numeros}')
+#print(f'O maior número é {maior} e o menor número é {menor}')
+print('Os valores sorteados foram: ', end='')
+for numero in numeros:
+    print(f'{numero} ', end='')
+print(f'\nO maior valor sorteado foi {max(numeros)}\nO menor valor sorteado foi {min(numeros)}')
 
-print(f'\nOs números sorteados foram: {numeros}')
-print(f'O maior número é {maior} e o menor número é {menor}')
