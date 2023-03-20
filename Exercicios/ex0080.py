57dfb79bd5999d024cdf97bfda9c46245e508cfc
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-20 18:17:20 -0300

    feat: listas - exercicio 80 nova solução

--- Exercicios/ex0080.py
+++ Exercicios/ex0080.py
@@ -2,13 +2,34 @@
 valores = []
 nwvalores = []
 # Cria a Lista
-for i in range(0, 5):
-    valor = int(input(f'Digite o {i + 1}° valor: '))
-    valores.append(valor)
+# for i in range(0, 5):
+#    valor = int(input(f'Digite o {i + 1}° valor: '))
+#    valores.append(valor)
 # Cria uma nova lista, inserindo a partir do menor valor e excluindo-o da lista anterior
+# for i in range(0, 5):
+#    nwvalores.append(min(valores))
+#    valores.remove(min(valores))
+# del valores
+# for valor in nwvalores:
+#    print(f'{valor} . ', end='')
+
 for i in range(0, 5):
-    nwvalores.append(min(valores))
-    valores.remove(min(valores))
-del valores
-for valor in nwvalores:
-    print(f'{valor} . ', end='')
+    valor = int(input(f'Digite o {i + 1}° valor:'))
+    if i == 0:
+        valores.append(valor)
+        print(f'{valor} adicionado ao final da lista')
+    elif valor > max(valores):
+        valores.append(valor)
+        print(f'{valor} adicionado ao final da lista')
+    elif valor < min(valores):
+        valores.insert(0, valor)
+        print(f'{valor} adicionado a posição 0')
+    else:
+        x = 1
+        while x < 4:
+            if valor < valores[x]:
+                valores.insert(x, valor)
+                print(f'{valor} adicionado a posição {x}')
+                break
+            x += 1
+print(valores)
