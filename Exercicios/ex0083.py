4938ef5193b4fa5c7b08883a9e974c75628874e8
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-20 19:58:43 -0300

    feat: listas - atualização exercicio 83

--- Exercicios/ex0083.py
+++ Exercicios/ex0083.py
@@ -1,11 +1,27 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parenteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta
 expressao = input('Digite uma expressão: ')
 lista = []
-for v in expressao:
-    lista.append(v)
-if lista.count('(') == 0 and lista.count(')') == 0:
-    print('Você não iniciou uma expressão!')
-elif lista.count('(') == lista.count(')'):
-    print('Sua expressão é válida!')
+# for v in expressao:
+#     lista.append(v)
+# if lista.count('(') == 0 and lista.count(')') == 0:
+#     print('Você não iniciou uma expressão!')
+# elif lista.count('(') == lista.count(')'):
+#     print('Sua expressão é válida!')
+# else:
+#     print('Sua expressão é inválida!')
+
+# A solução acima não pode ser considerada pq se estivar na ordem errad não vai detectar.
+for c in expressao:
+    if c == '(':
+        lista.append(c)
+    elif c == ')':
+        if len(lista) > 0:
+            if c == ")":
+                lista.pop()
+            else:
+                lista.append(c)
+                break
+if len(lista) == 0:
+    print('Sua expressão é válida')
 else:
-    print('Sua expressão é inválida!')
+    print('Sua expressão é Inválida')
