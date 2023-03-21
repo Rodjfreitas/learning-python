122ec856371fc864c55b133c1d3378652a77913a
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-21 18:05:02 -0300

    feat: listas - exercicio 85 - solução aproximada da exigida no enunciado

--- Exercicios/ex0085.py
+++ Exercicios/ex0085.py
@@ -1,17 +1,13 @@
 # Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. no final mostre os valores pares e impares em ordem crescente.
-numeros = list()
-pares = list()
-impares = list()
+numeros = [[], []]
 for c in range(0, 7):
     valor = int(input('Digite um valor: '))
     if valor % 2 == 0:
-        pares.append(valor)        
+        numeros[0].append(valor)
     else:
-        impares.append(valor)
-pares.sort()
-impares.sort()
-numeros.append(pares[:])
-numeros.append(impares[:])
+        numeros[1].append(valor)
+numeros[0].sort()
+numeros[1].sort()
 print(f'Pares: {numeros[0]}')
 print(f'Ímpares: {numeros[1]}')
 print(numeros)
