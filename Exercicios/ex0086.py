b106b9c7975a08aa817d75cad1896cd5ee46deeb
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-21 18:25:00 -0300

    feat: listas - exercicio 86

---
+++ Exercicios/ex0086.py
@@ -0,0 +1,8 @@
+# Crie um programa que crie uma matriz de dimensão 3 x 3 e preencha com valores lidos pelo teclado. No final mostre a matriz na tela com a formatação correta.
+matriz = [[], [], []]
+for c in range(0, 3):
+    for valor in range(0, 3):
+        matriz[c].append(int(input(f'Digite um número para [{c}, {valor}]: ')))
+print(f'\n{"MATRIZ 3X3":=^50}\n')
+for lista in matriz:
+    print(f'[ {lista[0]} ] [ {lista[1]} ] [ {lista[2]} ]')
