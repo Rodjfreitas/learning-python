d162cdbbe47ee5cf10d46664596f8639b834688c
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-25 12:27:55 -0300

    feat: dicionários - exercicio 95 correção

--- Exercicios/ex0095.py
+++ Exercicios/ex0095.py
@@ -1,4 +1,4 @@
-#Aprimore o desafio 93 para que funcione com vários jogadores, incluindo um sistema de visualização de detalhes de aproveitamento de cada jogador.
+# Aprimore o desafio 93 para que funcione com vários jogadores, incluindo um sistema de visualização de detalhes de aproveitamento de cada jogador.
 estatisticas = []
 dados = {}
 temporario = []
@@ -8,13 +8,14 @@ while True:
     print(f'\n{"-=":=^10} Cadastro Jogador {"-=":=^10}')
     dados['jogador'] = input('Nome do Jogador: ').strip().title()
     while dados['jogador'].isnumeric() == True:
-       dados['jogador'] = input('Inválido. Nome do Jogador: ').strip().title()
+        dados['jogador'] = input('Inválido. Nome do Jogador: ').strip().title()
     jogos = input(f'Quantos jogos foram disputados por {dados["jogador"]}? ')
     while jogos.isnumeric() == False:
-       jogos = input(f'Inválido. Quantos jogos foram disputados por {dados["jogador"]}? ')
+        jogos = input(
+            f'Inválido. Quantos jogos foram disputados por {dados["jogador"]}? ')
     for i in range(0, int(jogos)):
-       gols += int(input(f'gols marcados no jogo {i + 1}: '))
-       temporario.append(gols)
+        gols += int(input(f'gols marcados no jogo {i + 1}: '))
+        temporario.append(gols)
     dados['jogos'] = int(jogos)
     dados['gols'] = temporario[:]
     dados['totalGols'] = gols
@@ -25,20 +26,25 @@ while True:
     dados.clear()
     continuar = input('Cadastrar novo jogador? [S/N]: ').strip().upper()
     while continuar not in 'SN':
-       continuar = input('Inválido. Cadastrar novo jogador? [S/N]: ').strip().upper()
+        continuar = input(
+            'Inválido. Cadastrar novo jogador? [S/N]: ').strip().upper()
     if continuar == 'N':
-       break
-print(estatisticas)
+        break
 print('-=' * 50)
 print(f'{"cod":>3} {"nome":<20} {"jogos":<5} {"Média":<5} {"Total":<5} {"gols":<25}')
-for pos, valor in enumerate (estatisticas):
-   print(f'{pos:>3} {valor["jogador"]:<20} {valor["jogos"]:<5} {valor["mediaGols"]:>5} {valor["totalGols"]:>5} {valor["gols"]}')
+for pos, valor in enumerate(estatisticas):
+    print(
+        f'{pos:>3} {valor["jogador"]:<20} {valor["jogos"]:<5} {valor["mediaGols"]:>5.1f} {valor["totalGols"]:>5} {valor["gols"]}')
 while True:
-   jogador = int(input('\nMostrar dados de qual jogador? '))
-   if jogador > len(estatisticas):
-      print('Jogador Inexistente')
-      break
-   print(f'Levantamento do jogador {estatisticas[jogador]["jogador"]}')
-   for i in range(0, len(estatisticas[jogador]["gols"])):
-      print(f'  No jogo {i + 1} fez {estatisticas[jogador]["gols"][i]}')
-print(f'{"FIM":=^20}')
+    jogador = int(input('\nMostrar dados de qual jogador? '))
+    if jogador != 999 and jogador < len(estatisticas):
+        print(f'Levantamento do jogador {estatisticas[jogador]["jogador"]}')
+        for i in range(0, len(estatisticas[jogador]["gols"])):
+            print(f'  No jogo {i + 1} fez {estatisticas[jogador]["gols"][i]} gols.')
+    elif jogador == 999:
+        break
+    else:
+        print('Jogador Inexistente')
+
+
+print(f'{"FIM":=^50}')
