dbc612886559ec7b99bebf9a21441b0f587dcb2a
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-25 16:20:56 -0300

    feat: funções - exercicio 96

---
+++ Exercicios/ex0096.py
@@ -0,0 +1,23 @@
+# Faça um programa que tenha uma função chamada area(), que receba as dimensoes de um terreno retangular (largura e comprimento) e mostre a area do terreno.
+def area(l, c):
+    area = float(l * c)
+    print(f'O terreno possui {area:.1f}m²')
+
+
+def linha():
+    print('-=' * 20)
+
+
+while True:
+    linha()
+    largura = float(input('Informe a largura do terreno: '))
+    comprimento = float(input('Informe o comprimento do terreno: '))
+    area(largura, comprimento)
+    while True:
+        continuar = input('\nNovo calculo? [S/N]').upper()[0]
+        if continuar in 'SN':
+            break
+        print('Opção Inválida!')
+    if continuar == 'N':
+        break
+linha()
