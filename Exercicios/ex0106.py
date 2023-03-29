4e87666df89a25a5b8fd8bbe237d618a6c8c072d
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-03-28 22:20:00 -0300

    feat: funções - atualização ex106

--- Exercicios/ex0106.py
+++ Exercicios/ex0106.py
@@ -1,32 +1,40 @@
 # Faça um mini sistema que utilize o interactive help do python. O usuário vai digitar o comando e o manual vai aparecer. quando o usuário digitar a palavra fim, o programa se encerrará. Obs: use cores.
+from time import sleep
+
+c = ('\033[m)',  # 0 sem cor
+     '\033[0;30;41m', # 1 Vermelho
+     '\033[0;30;42m', # 2 Verde
+     '\033[0;30;43m', # 3 Amarelo
+     '\033[0;30;44m', # 4 Azul
+     '\033[0;30;45m', # 5 Roxo
+     '\033[7;30m'     # 6 Branco
+     )
 
 
-def ajuda():
-    principal = '\033[1;30;42m'
-    acessando = '\033[1;30;46m'
-    resposta = '\033[7;37;40m'
-    fim = '\033[1;30;41m'    
-    while True:
-        titulo('SISTEMA DE AJUDA PyHELP', principal)
-        hp = input('Função ou Biblioteca: ').strip().lower()
-        if hp == 'fim':
-            titulo('Até logo', fim)
-            break
-        titulo(f'Acessando o manual do comando "{hp}"', acessando)
-        textAjuda(help(hp), resposta)
-    
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
 
 
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[cor], end='')
+    sleep(1)
 
 
-def titulo(msg, cor):
-    print(f'{cor}~' * len(msg), f'{cor} ' * 50)
-    print(f'{cor}{msg}', f'{cor} ' * 50)
-    print(f'{cor}~' * len(msg), f'{cor} ' * 49, '\033[m')
-
-
-def textAjuda(msg, cor):
-    print(f'{cor}{msg}\033[m')
-
-
-ajuda()
+# Programa Principal
+comando = ' '
+while True:
+    titulo('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input('Função ou Biblioteca >'))
+    if comando.upper() == 'FIM':
+        break
+    ajuda(comando)
+titulo('Até Logo', 1)
