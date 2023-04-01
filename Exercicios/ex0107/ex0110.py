75c4cb13c2f17180723004b5aeda5fdb94f4774a
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-04-01 15:13:45 -0300

    feat: modulos, pacotes - exercicio 110

---
+++ Exercicios/ex0107/ex0110.py
@@ -0,0 +1,7 @@
+import moeda
+# adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos criado até aqui
+x = False
+numero = float(input('Digite o valor: '))
+add = float(input('Aumento: '))
+red = float(input('Redução: '))
+moeda.resumo(numero, add, red)
