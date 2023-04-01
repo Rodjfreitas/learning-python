560f2576a55bbdc9ad8dce16bd7c62597b3c3b87
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-04-01 12:01:59 -0300

    feat: modulos

---
+++ Modulos_Pacotes/aula22.py
@@ -0,0 +1,10 @@
+def fatorial(n):
+    f = 1
+    for c in range(1, n + 1):
+        f *= c
+    return f
+
+
+num = int(input('Digite um número: '))
+fat = fatorial(num)
+print(f'O fatorial de {num} é igual à {fat}')
