b2d2279573af0abf7a2045295a6f36e42740b3c0
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-04-04 20:02:16 -0300

    feat: exercicio 111

--- Exercicios/ex0107/ex0111.py
+++ Exercicios/ex0107/ex0111.py
@@ -1,8 +1,7 @@
+from utilidadesCeV import moeda
 # Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamado moeda e dado.
 # Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando
-import ex0107.utilidadesCeV
 # adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos criado até aqui
 numero = float(input('Digite o valor: '))
 add = float(input('Aumento: '))
 red = float(input('Redução: '))
-ex0107.utilidadesCeV.moeda.resumo(numero, add, red)
